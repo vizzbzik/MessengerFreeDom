77809a53286288f73c4a7a89a0af22cd55fa9259
vizzbzik <jimkoye@yandex.ru>
2025-07-28 11:25:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,11 +2,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
